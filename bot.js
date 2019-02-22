a90aec5833c08e9d632acfccc73fee636b6b1ee6
3bker <44803452+3bker@users.noreply.github.com>
2019-02-22 23:01:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,4 +19,15 @@ client.on("message", function(message) {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
+
+let PREFIX = '-'
+  if (command === "say") {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+          message.delete()
+    message.channel.sendMessage(args.join(" "))
+  }
+  
+ 
+
 client.login(process.env.BOT_TOKEN); 
